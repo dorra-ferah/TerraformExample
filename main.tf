6c3d1713abef0943a2265d6e1bdccdc616fcffa0
dorra-ferah <123673334+dorra-ferah@users.noreply.github.com>
2023-08-29 15:05:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_virtual_machine" "dorra" {
-  name             = "dorra"
+resource "vsphere_virtual_machine" "dorratoutou" {
+  name             = "dorratoutou"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   folder = "Dorra Ferah"
   datastore_id     = data.vsphere_datastore.datastore.id
